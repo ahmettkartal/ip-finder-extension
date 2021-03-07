b2156d49ca1b56d0acf33c399c3fbc3bf2c8e0d1
ahmettkartal <ahmettkartal@yahoo.com>
2021-03-07 17:07:07 +0300

    request error control

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 document.getElementById("find").onclick = function() {
     // Create Request
-    var xmlHttp = new XMLHttpRequest();
+    let xmlHttp = new XMLHttpRequest();
 
     // Request to ipify for ip information
     xmlHttp.open( "GET", "https://api.ipify.org?format=json", false ); // false for synchronous request
     xmlHttp.send( null );
 
-    // Cast request response to JSON object
-    var obj = JSON.parse(xmlHttp.responseText);
-
-    // Display IP information to our extension
-    document.getElementById("result").innerHTML = obj.ip;
+    try{
+        // Cast request response to JSON object
+        let obj = JSON.parse(xmlHttp.responseText);
+        // Display IP information to our extension
+        document.getElementById("result").innerHTML = obj.ip;
+    }catch(e){
+        // Display error information to our extension
+        document.getElementById("result").innerHTML = "An error occured";
+    }
 };
 
